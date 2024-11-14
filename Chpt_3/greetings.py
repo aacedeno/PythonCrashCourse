c0115437df16a9c621e6bf4ade89c61744b5f3c0
aceden <adrian_cedeno@anfcorp.com>
2024-11-13 18:16:26 -0600

    More examples

---
+++ Chpt_3/greetings.py
@@ -0,0 +1,11 @@
+#: Start with the list, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name
+
+
+names = ['Ryan', 'Jhon', 'Zahir', "Monica"]
+
+message_1 = f'Hello {names[0]}, how are you?'
+message_2 = f'Hello {names[3]}, how are you?'
+
+
+print(message_1)
+print(message_2)
---
+++ Chpt_3/update_guest_lists.py
@@ -0,0 +1,17 @@
+
+#If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+#invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+guest_list = ["MJ", "Messi", "Nelson"]
+
+print(f"Hello, {guest_list[0]}, come join me for dinner")
+
+print(f"Hello, {guest_list[1]}, come join me for dinner")
+
+print(f"Hello, {guest_list[2]}, come join me for dinner")
+
+print(f"{guest_list[2]} is not able to attend")
+
+guest_list[2] = "Lebron"
+
+print(guest_list)
